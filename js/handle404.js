f74992d6b836734b43e112dd1a195909e5af8548
colinator27 <colinator27@users.noreply.github.com>
2018-07-13 12:38:20 -0400

    More legacy browser support

--- js/handle404.js
+++ js/handle404.js
@@ -19,46 +19,46 @@ $(document).ready(function(){
     dataType: 'json',
     success: function(data){
       // Load the default index.html stuff, that hasn't yet been loaded
-      document.body.innerHTML = `\
-    <div id="verytop">
-      <div class="vb"></div>
-      <p class="tc dfont sdouble">colinator27</p>
-    </div>
-    <div class="navbar">
-      <div class="container">
-        <div class="col-sm-12 wline"></div>
-        <div class="col-sm-3 fullsize">
-          <a class="tc dfont fullsize" href="https://gamejolt.com/@colinator27">GAMEJOLT</a>
-        </div>
-        <div class="col-sm-3 fullsize">
-          <a class="tc dfont fullsize" href="https://github.com/colinator27">GITHUB</a>
-        </div>
-        <div class="col-sm-3 fullsize">
-          <a class="tc dfont fullsize" href="https://www.youtube.com/user/colinator27/">YOUTUBE</a>
-        </div>
-        <div class="col-sm-3 fullsize">
-          <a class="tc dfont fullsize" href="https://www.reddit.com/user/colinator27/">REDDIT</a>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <div id="content-area">
-        <p>Loading...</p>
-      </div>
-    </div>`;
+      document.body.innerHTML = '\
+    <div id="verytop">\
+      <div class="vb"></div>\
+      <p class="tc dfont sdouble">colinator27</p>\
+    </div>\
+    <div class="navbar">\
+      <div class="container">\
+        <div class="col-sm-12 wline"></div>\
+        <div class="col-sm-3 fullsize">\
+          <a class="tc dfont fullsize" href="https://gamejolt.com/@colinator27">GAMEJOLT</a>\
+        </div>\
+        <div class="col-sm-3 fullsize">\
+          <a class="tc dfont fullsize" href="https://github.com/colinator27">GITHUB</a>\
+        </div>\
+        <div class="col-sm-3 fullsize">\
+          <a class="tc dfont fullsize" href="https://www.youtube.com/user/colinator27/">YOUTUBE</a>\
+        </div>\
+        <div class="col-sm-3 fullsize">\
+          <a class="tc dfont fullsize" href="https://www.reddit.com/user/colinator27/">REDDIT</a>\
+        </div>\
+      </div>\
+    </div>\
+    <div class="container">\
+      <div id="content-area">\
+        <p>Loading...</p>\
+      </div>\
+    </div>';
       processPageData(data, path);
     },
     error: function(xhr, textStatus, errorThrown){
       // Page not found, throw 404 error for real
       document.title = 'colinator27 - 404';
-      document.body.innerHTML = `\
-    <center>
-      <div class="vb"></div>
-      <h1 class="dfont sdouble">Wrong page!</h1>
-      <br>
-      <br>
-      <p class="dfont">Click <a href="https://colinator27.github.io/" style="color:#a00;">here</a> to go back to the homepage.</p>
-    </center>`;
+      document.body.innerHTML = '\
+    <center>\
+      <div class="vb"></div>\
+      <h1 class="dfont sdouble">Wrong page!</h1>\
+      <br>\
+      <br>\
+      <p class="dfont">Click <a href="https://colinator27.github.io/" style="color:#a00;">here</a> to go back to the homepage.</p>\
+    </center>';
     }
   });
 });
